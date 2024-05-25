644eaca4a771836c2fc53645dee7b5945ee094ad
Vikas Shah <vikasshah508@gmail.com>
2024-05-25 12:10:00 +0530

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { addheaddata,createElement,loadcontent } from "./function.js";
+import { addheaddata,createElement,loadcontent } from "scripts/function.js";
 
 //#region dom content laod event
 
